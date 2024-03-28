4d96b2d95f6d2ba38984d6500ee4c7c4993990d9
deepakkremiges <deepak.kumar@remiges.tech>
2024-03-28 12:05:40 +0530

    Added payload

---
+++ assignment/src/main/java/com/deepak/assignment/payload/User.java
@@ -0,0 +1,17 @@
+package com.deepak.assignment.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+}
