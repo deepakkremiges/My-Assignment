f6dd3f6a02fe88d331913608f298be82ffc866ee
deepakkremiges <deepak.kumar@remiges.tech>
2024-03-28 13:17:43 +0530

    add RequestMapping

--- assignment/src/main/java/com/deepak/assignment/controller/KafkaProducerController.java
+++ assignment/src/main/java/com/deepak/assignment/controller/KafkaProducerController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.deepak.assignment.kafka.JsonKafkaProducer;
 import com.deepak.assignment.payload.User;
 
 @RestController
+@RequestMapping("/api/v1/kafka")
 public class KafkaProducerController {
 
     @Autowired
